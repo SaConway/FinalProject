36da8031c2abd4008ab067f1599e07bce39269d6
Idan <idanmarc91@gmail.com>
2018-02-22 14:59:05 +0200

    test 1

--- MsorLi/MsorLi/Views/ItemList.xaml.cs
+++ MsorLi/MsorLi/Views/ItemList.xaml.cs
@@ -4,7 +4,7 @@ using System;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
+//hello test
 namespace MsorLi.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
