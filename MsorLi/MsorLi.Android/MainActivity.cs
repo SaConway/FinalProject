768492183f96a20fbb2f4c9161713ccd2094cfc7
sageeconway <sageeconway@gmail.com>
2018-02-22 18:04:41 +0200

    .

--- MsorLi/MsorLi.Android/MainActivity.cs
+++ MsorLi/MsorLi.Android/MainActivity.cs
@@ -1,14 +1,9 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
-using Android;
 
-namespace MsorLi.Android
+namespace MsorLi.Droid
 {
     [Activity(Label = "MsorLi", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
