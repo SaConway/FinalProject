77f4c0fe3957b3d80ad7af9ee8771905c5710c61
Idan <idanmarc91@gmail.com>
2018-02-25 19:22:33 +0200

    idan

--- MsorLi/MsorLi.Backend/Controllers/ItemController.cs
+++ MsorLi/MsorLi.Backend/Controllers/ItemController.cs
@@ -15,14 +15,22 @@ namespace MsorLi.Backend.Controllers
         {
             base.Initialize(controllerContext);
             MasterDetailContext context = new MasterDetailContext();
+
             DomainManager = new EntityDomainManager<Item>(context, Request);
+
+            //if we would like to do "soft delete" we need to use the next command:
+            //DomainManager = new EntityDomainManager<Item>(context, Request, enableSoftDelete: true);
+            //soft delete is a flag that we use for each column in the table.
+            //this flag is used as a way to delete data without actually deleting it.
+            //we use the flag as a way to know if the content is deleted or not.
+            //this way we could also recover deleted items
         }
 
         // GET tables/Item
         public IQueryable<Item> GetAllItems()
         {
             return Query();
-        }
+        } 
 
         // GET tables/Item/48D68C86-6EA6-4C25-AA33-223FC9A27959
         public SingleResult<Item> GetItem(string id)
@@ -37,6 +45,7 @@ namespace MsorLi.Backend.Controllers
         }
 
         // POST tables/Item
+        //insert into the data base
         public async Task<IHttpActionResult> PostItem(Item item)
         {
             Item current = await InsertAsync(item);
