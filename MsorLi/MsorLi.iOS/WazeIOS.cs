f74cb4ef2560c2d00ed01339b3049ef8090a4274
Idan Marciano <idanmarc91@gmail.com>
2018-05-05 18:50:00 +0300

    fix waze on ios, changed image in saveditempage to round picture

--- MsorLi/MsorLi.iOS/WazeIOS.cs
+++ MsorLi/MsorLi.iOS/WazeIOS.cs
@@ -3,6 +3,7 @@ using MsorLi.iOS;
 using UIKit;
 using Foundation;
 using System;
+using System.Web;
 
 [assembly: Dependency(typeof(WazeIOS))]
 namespace MsorLi.iOS
@@ -13,14 +14,30 @@ namespace MsorLi.iOS
         {
             try
             {
+                //HttpUtility x;
+                string url_encoded_location = System.Net.WebUtility.UrlEncode("ירושלים");
                 // Launch Waze to look for location:
-                UIApplication.SharedApplication.OpenUrl(new NSUrl("https://waze.com/ul?q=" + location));
+                UIApplication.SharedApplication.OpenUrl(new NSUrl("https://waze.com/ul?q="));
+
+              //  var uri = new Uri("https://waze.com/ul?q=ירושלים");
+              //  var nsurl = new NSUrl(uri.GetComponents(UriComponents.HttpRequestUrl, UriFormat.UriEscaped));
+              //// UIApplication.SharedApplication.OpenUrl(nsurl);
+                //var webView = new WebView
+                //{
+                //    Source = "https://waze.com/ul?q=ירושלים",
+                //    HeightRequest = 1
+                //};
+
+
             }
             catch (Exception)
             {
                 // If Waze is not installed, open it in Itunes:
                 UIApplication.SharedApplication.OpenUrl(new NSUrl("http://itunes.apple.com/us/app/id323229106"));
             }
+
+
+   
         }
     }
 }
