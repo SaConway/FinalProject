d390959ff6cb0ce5704f0afd2a861cabfc8ae9e6
Idan Marciano <idanmarc91@gmail.com>
2018-05-25 14:31:41 +0300

    app test cloud

--- MsorLi/MsorLi.Android/obj/Release/designtime/Resource.Designer.cs
+++ MsorLi/MsorLi.Android/obj/Release/designtime/Resource.Designer.cs
@@ -24,10 +24,6 @@ namespace MsorLi.Droid
 			global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 		}
 		
-		public static void UpdateIdValues()
-		{
-		}
-		
 		public partial class Drawable
 		{
 			
